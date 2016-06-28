4864ce8babd51eff235642d92947730e51377cc4
petryshend <petryshend@gmail.com>
2016-06-28 18:24:33 +0300

    Basic chat functionality implemented

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,14 @@
+var socket = io();
+
 $(document).ready(function() {
-    
+    $('form').submit(function() {
+        var newMessageInput = $('#new-message');
+        socket.emit('chat message', newMessageInput.val());
+        newMessageInput.val('');
+        return false;
+    });
+
+   socket.on('chat message', function(data) {
+        $('#messages').append($('<li>').text(data));
+    });
 })
